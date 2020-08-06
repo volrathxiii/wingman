5461685ad00363de9224f6a15ec23b1206021454
Patrick Rivera <patriv@jpj.com>
2020-08-06 10:59:41 +0800

    fix loading of workers

--- src/worker.server.ts
+++ src/worker.server.ts
@@ -19,41 +19,8 @@ pm2.connect((err)=>{
       script    : workerList[workerName],         // Script to be run
       watch: [workerList[workerName]],
       wait_ready: true,
-      // exec_mode : 'cluster',        // Allows your app to be clustered
-      // instances : 4,                // Optional: Scales your app by 4
-      // max_memory_restart : '100M'   // Optional: Restarts your app if it reaches 100Mo
     }, function(err, apps) {
-      // pm2.disconnect();   // Disconnects from PM2
       if (err) throw err
     });
   })
-
-  // pm2.start({
-  //   name: 'test',
-  //   script    : './build/workers/autosleep.worker.js',         // Script to be run
-  //   // exec_mode : 'cluster',        // Allows your app to be clustered
-  //   // instances : 4,                // Optional: Scales your app by 4
-  //   // max_memory_restart : '100M'   // Optional: Restarts your app if it reaches 100Mo
-  // }, function(err, apps) {
-  //   // pm2.disconnect();   // Disconnects from PM2
-  //   if (err) throw err
-  // });
-
-  // pm2.start({
-  //   name: 'test2',
-  //   script    : './build/workers/autosleep.worker.js',         // Script to be run
-  //   // exec_mode : 'cluster',        // Allows your app to be clustered
-  //   // instances : 4,                // Optional: Scales your app by 4
-  //   // max_memory_restart : '100M'   // Optional: Restarts your app if it reaches 100Mo
-  // }, function(err, apps) {
-  //   // pm2.disconnect();   // Disconnects from PM2
-  //   if (err) throw err
-  // });
 })
-
-// let workerList = Workers.all()
-// console.log(workerList,'--list')
-// Object.keys(workerList).forEach(workerName=>{
-//   console.log(workerList[workerName], '--file')
-  
-// })
